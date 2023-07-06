22f0f3549790e66b7da699d050615b2ed6f1f14d
yusurMk <130349202+yusurMk@users.noreply.github.com>
2023-07-06 19:02:31 +0000

    ddd, Jul 7/6/2023, YYYY, 7:02 A +0

--- hello.c
+++ hello.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-  printf(hello, world")
-
-  // Step ( 1 ) : Print Hello, World
-
-
-
-  //Step ( 2 ) : Print Hello, Your Name
-
+   string answer= get_string ("waht's your name");
+   printf(" hello %s\n", answer );
 }
