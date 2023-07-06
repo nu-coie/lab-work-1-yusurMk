fcf205c847c820a06a6d54a6599ec8fd4e3bbc06
yusurMk <130349202+yusurMk@users.noreply.github.com>
2023-07-06 19:48:52 +0000

    ddd, Jul 7/6/2023, YYYY, 7:48 A +0

--- hello.c
+++ hello.c
@@ -4,7 +4,7 @@
 int main(void)
 {
 int num1= get_int ("enter the first number");
-int num2 = get_int("enter the second number");
+int num2= get_int("enter the second number");
 int result = num1 + num2;
 printf("%d\n", result);
 }
