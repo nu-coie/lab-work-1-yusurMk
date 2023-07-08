512705490e0506691cb8f7be62facd14e6d50798
yusurMk <130349202+yusurMk@users.noreply.github.com>
2023-07-08 11:23:20 +0000

    ddd, Jul 7/8/2023, YYYY, 11:23 A +0

--- calculator.c
+++ calculator.c
@@ -8,7 +8,8 @@ int main(void)
  int num1= get_intger("enter the first number");
  int num2=get_intger("enter the second number");
  char method=get_char("choose the method");
- method=("A,M,D");
-if(A)
+ method=("A,S,M,D");
+if(method==A || B)
+printf("%i\n, num1_num2);
 
 }
