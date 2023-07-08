68bdc525a7c1f55788b67e8ec5383721a158634c
yusurMk <130349202+yusurMk@users.noreply.github.com>
2023-07-08 11:16:57 +0000

    ddd, Jul 7/8/2023, YYYY, 11:16 A +0

--- calculator.c
+++ calculator.c
@@ -5,7 +5,9 @@
 int main(void)
 {
 // Accept Input From user (char), where (a) stands for "Addition", (m) for "Multiplications," and (d) for "Division,"
- 
+ int a= get_intger("enter the first number");
+ int b=get_intger("enter the second number");
+ char method=get_char("A)
 
 
 }
