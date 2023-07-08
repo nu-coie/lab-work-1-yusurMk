16faf0ecca3151faf3507291d695298b5741dcea
yusurMk <130349202+yusurMk@users.noreply.github.com>
2023-07-08 12:44:30 +0000

    ddd, Jul 7/8/2023, YYYY, 12:44 A +0

--- calculator.c
+++ calculator.c
@@ -5,23 +5,17 @@
 int main(void)
 {
 // Accept Input From user (char), where (a) stands for "Addition", (m) for "Multiplications," and (d) for "Division,"
- int num1= get_int("enter the first number");
- int num2=get_int("enter the second number");
- char method=get_char("choose the method (A,M,D): ");
-
-if(method=='A' || method=='a' )
+ int num1=get_int("write the first number");
+ int num2=get_int("write the second number");
+ char method=get_char("choose the method (A,M,D)");
+ if(method=='A' || method=='a')
+ {printf("%i\n", num1+num2);
+ }
+ if(method=='M' || method=='m')
+ { printf("%i\n", num1*num2);
+ }
+if(method=='D' || method=='d')
 {
-    printf("%i\n", num1+num2);
-
-}
-    if(method=='M' || method=='m')
-
-{
-    printf("%i\n", num1*num2);
-}
-
-    if(method=='D' || method=='d')
-    {
-printf("%i\n",num1%num2);
+    printf("%i\n", num1%num2);
 }
 }
